943943f099c7fc94fcc70114f0a3e5e116afa7b3
GUSTAVO DI RISIO <spgustavorisio@gmail.com>
2024-02-02 23:06:44 -0300

    Atualizacoes

---
+++ javascript-exercicios/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+if  (vel > 60) { //Condição simples!
+    console.log(`Voce ultrapassou a velocidade permita. Você foi multado!`)
+}
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+console.log(`Digite sempre usando o cinto de segurança!`)
