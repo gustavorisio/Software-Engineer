71077497433baae4cb2bed47175fed87d01c63cd
GUSTAVO DI RISIO <spgustavorisio@gmail.com>
2024-08-11 14:01:35 -0300

    atualizacoes java

--- JAVA/java-exercicios-curso/ex_3_binarios.java
+++ JAVA/java-exercicios-curso/ex_3_binarios.java
@@ -7,7 +7,15 @@ Este código foi digitado por Gustavo Di Risio | github.com/gustavorisio | linke
    O operador de módulo (%) retorna o resto da divisão entre dois operandos inteiros. 
    Os operadores &, | e ^ implementam operações orientada para bits, operações que atuam sobre os bits individuais dos operandos. 
    O operador & realiza a operação lógica E entre cada bit individual dos operandos.
-
+   O operador | realiza a operação lógica OU entre cada bit individual dos operandos.
+   O operador ^ realiza a operação lógica XOU (ou exclusivo) entre cada bit individual dos operandos.
+   Os operadores <<, >> e >>> implementam operações de deslocamento de bits. 
+   Os bits do operando à esquerda serão deslocados o número de posições especificadas pelo operando à direita. 
+   Os bits que saem dos limites do campo são perdidos e a variável é preenchida com zeros no lado oposto ao deslocamento.  
+   O operador << realiza a operação de deslocamento de bits a esquerda. 
+   Os operadores >> e >>> realizam a operação de deslocamento de bits a direita. A diferença entre o operador >> e o >>> é que o primeiro não desloca o bit de mais alta ordem usado para indicar números negativos. 
+   
+   O programa abaixo mostra o uso desses operadores:
  */
 
  public class ex_3_binarios {
---
+++ JAVA/java-exercicios-curso/ex_4_relacionais.java
@@ -0,0 +1,21 @@
+/*
+Este código foi digitado por Gustavo Di Risio | github.com/gustavorisio | linkedin.com/in/gustavorisio
+*/
+/*
+
+    Os operadores relacionais são usados para comparar valores. O resultado da aplicação desses operadores é um valore boolano, ou seja :true ou false. 
+    O programa abaixo mostra o uso desses operadores
+
+ */
+public class ex_4_relacionais {
+    public static void main (String args[]) {
+       int x = 7, y = 8;
+       System.out.println("x = " + x);
+       System.out.println("y = " + y);
+       System.out.println("x < y = " + (x < y));
+       System.out.println("x > 10 = " + (x > 10));
+       System.out.println("y <= 8 = " + (y <= 8));
+       System.out.println("y == 5 = " + (y == 5));
+       System.out.println("x != y = " + (x != y));
+     }
+    }
