ecc9e7d24de38916693fc19b2df387b9b6be749c
GUSTAVO DI RISIO <spgustavorisio@gmail.com>
2024-08-11 12:55:28 -0300

    atualizacoes

---
+++ JAVA/java-exercicios-curso/exercicio-1-OlaMundo.java
@@ -0,0 +1,17 @@
+/*
+Este código foi digitado por Gustavo Di Risio | github.com/gustavorisio | linkedin.com/in/gustavorisio
+*/
+
+package main;
+
+public class OlaMundo {
+
+    public void exibeOla() {
+        System.out.println("Ola, Mundo!");
+    }
+
+    public static void main(String args[]) {
+        OlaMundo obj = new OlaMundo();
+        obj.exibeOla();
+    }
+}
