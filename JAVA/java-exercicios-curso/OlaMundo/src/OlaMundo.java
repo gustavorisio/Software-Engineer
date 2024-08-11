764a5ad586f161950ce61d1856f1f68bed5cd1bf
GUSTAVO DI RISIO <spgustavorisio@gmail.com>
2024-08-11 12:44:03 -0300

    atualizacoes

---
+++ JAVA/java-exercicios-curso/OlaMundo/src/OlaMundo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author REDRAGON
+ */
+public class OlaMundo {
+
+    public void exibeOla() {
+        System.out.println("Ola, Mundo!");
+    }
+
+    public static void main(String args[]) {
+        OlaMundo obj = new OlaMundo();
+        obj.exibeOla();
+    }
+}
