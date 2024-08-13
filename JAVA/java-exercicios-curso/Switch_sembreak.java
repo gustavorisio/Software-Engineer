60b669bc7e46274db628cfb299b58564649062fb
GUSTAVO DI RISIO <spgustavorisio@gmail.com>
2024-08-13 12:35:07 -0300

    Atualizacoes

---
+++ JAVA/java-exercicios-curso/Switch_sembreak.java
@@ -0,0 +1,29 @@
+/*
+Este código foi digitado por Gustavo Di Risio | github.com/gustavorisio | linkedin.com/in/gustavorisio
+*/
+/* 
+   O comando switch é útil quando existem várias ações distintas que
+   precisam ser realizadas em função do resultado da avaliação de uma expressão.
+   Não é nada que não poderia ser feito com um conjunto de if-else, no entanto o
+   uso do switch facilita codificação e a legibilidade do programa.
+
+   Neste código mostra as saídas de um programa com um comando switch que não faz uso do comando break. 
+   Note que todos os comandos são executados, uma vez que não existe um comando break para interromper a execução.
+   A omissão do comando break pode ser útil para poupar digitação naqueles casos onde mais de uma opção precisa executar a mesma sequência de comandos.
+ */
+public class Switch_sembreak {
+   public static void main (String args[]) {
+       char letra = 'i';
+       switch(letra)
+       {
+       case 'i':
+    System.out.println("inserir");
+       case 'a':
+       case 'e':
+    System.out.println("alterar");
+    System.out.println("excluir");
+       default: System.out.println(
+               "Ação ignorada: "+letra);
+       }
+     }
+}
