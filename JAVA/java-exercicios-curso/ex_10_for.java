bd6498562b792c22d0083a2278ca18f530415562
GUSTAVO DI RISIO <spgustavorisio@gmail.com>
2024-08-16 13:07:03 -0300

    Atualizacoes

---
+++ JAVA/java-exercicios-curso/ex_10_for.java
@@ -0,0 +1,14 @@
+/*
+Este código foi digitado por Gustavo Di Risio | github.com/gustavorisio | linkedin.com/in/gustavorisio
+*/
+/*
+    O laço for é uma estrutura de repetição muito comum em programação, usada para executar um bloco de código um número determinado de vezes.
+*/
+public class ex_10_for {
+    public static void main(String[] args) {
+        int x = 0;
+        for (int i = 1; i <= 100; i++)
+            x += i;
+        System.out.println("A soma de 1 a 100 é: " + x);
+    }
+}
