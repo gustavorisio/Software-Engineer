5def3f7903be8a6153b91082cef0e40bb58a335b
GUSTAVO DI RISIO <spgustavorisio@gmail.com>
2024-08-23 13:25:27 -0300

    Atualizacoes

---
+++ JAVA/java-exercicios-curso/ex_11_condicional.java
@@ -0,0 +1,16 @@
+/*
+Este código foi digitado por Gustavo Di Risio | github.com/gustavorisio | linkedin.com/in/gustavorisio
+*/
+public class ex_11_condicional
+{ // classe para teste do operador ?:
+public static void main (String args[])
+ {
+int x = 5;  // variável usada para o teste
+/*
+  Será impresso true se x for par
+*/
+boolean par = (x % 2 == 0) ? true : false;
+System.out.println("x = " + x);
+System.out.println("É par = " + par);
+ }
+}
