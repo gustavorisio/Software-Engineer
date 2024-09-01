7ab98f9d95a6fdfd637a23faba3464748ad630f6
gustavorisio <spgustavorisio@gmail.com>
2024-08-31 22:07:02 -0300

    atualizacoes

--- JAVASCRIPT/js-exercicios-faculdade/EX3_APP/src/server.ts
+++ JAVASCRIPT/js-exercicios-faculdade/EX3_APP/src/server.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import { router } from "./routes";
 
 const app = express();
-
 app.use(express.json());
+app.use(router);
 
 console.log("Start Server:3000");
 app.listen(3000);
