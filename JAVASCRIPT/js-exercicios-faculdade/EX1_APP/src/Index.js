7235bfd12bf2fb3adc58752bb2852f7530ae5f8e
gustavorisio <spgustavorisio@gmail.com>
2024-08-31 21:22:18 -0300

    atualizacoes

--- JAVASCRIPT/js-exercicios-faculdade/EX1_APP/src/Index.js
+++ JAVASCRIPT/js-exercicios-faculdade/EX1_APP/src/Index.js
@@ -1,3 +1,10 @@
 const express = require('express');
 
 const app = express();
+
+app.get("/", (request, response) => {
+    return response.send("API no ar");
+});
+
+console.log("Start Server:3000");
+app.listen(3000);
