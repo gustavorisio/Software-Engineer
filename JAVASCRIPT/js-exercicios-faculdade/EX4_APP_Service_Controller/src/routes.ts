08673d7714edab8be6ec980439805846a4084d3b
gustavorisio <spgustavorisio@gmail.com>
2024-08-31 23:26:36 -0300

    atualizacoes

--- JAVASCRIPT/js-exercicios-faculdade/EX4_APP_Service_Controller/src/routes.ts
+++ JAVASCRIPT/js-exercicios-faculdade/EX4_APP_Service_Controller/src/routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { CreateUserController } from "./controller/user/CreateUserController";
 import { ListUsersController } from "./controller/user/ListUsersController";
+import { UpdateUserController } from "./controller/user/UpdateUserController";
 
 const router = Router();
 const createUserController = new CreateUserController();
 const listUsersController = new ListUsersController();
+const updateUserController = new UpdateUserController();
 
 router.post("/users", createUserController.handle);
 router.get("/users", listUsersController.handle);
+router.put("/users/:id", updateUserController.handle);
 
 export {router};
