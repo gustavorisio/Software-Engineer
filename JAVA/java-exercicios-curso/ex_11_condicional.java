6e0062f9a92f871d64828470c183b163c3ac0048
gustavorisio <spgustavorisio@gmail.com>
2024-09-02 13:32:19 -0300

    atualizacoes

--- JAVA/java-exercicios-curso/ex_11_condicional.java
+++ JAVA/java-exercicios-curso/ex_11_condicional.java
@@ -6,9 +6,7 @@ public class ex_11_condicional
 public static void main (String args[])
  {
 int x = 5;  // variável usada para o teste
-/*
-  Será impresso true se x for par
-*/
+//  Será impresso true se x for par
 boolean par = (x % 2 == 0) ? true : false;
 System.out.println("x = " + x);
 System.out.println("É par = " + par);
